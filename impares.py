5c9f34eedcff05c518e23608fd3a5d8fc0a35354
emmaalbarran <80660627+emmaalbarran@users.noreply.github.com>
2022-08-19 00:01:00 -0400

    Update impares.py

--- impares.py
+++ impares.py
@@ -1,1 +1,13 @@
 
+
+from functools import reduce
+
+lista = [2, 4, 3, 7, 5, 8, 10]
+salida1 = list(filter(lambda x: (x % 2 != 0), lista))
+print("Números impares usando la expresión lambda: ", salida1 )
+
+def sum(a, b):
+    return a + b
+
+total = reduce(sum, salida1)
+print(total)
